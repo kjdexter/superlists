689b7f40bdd8faf729f4a460ed33ba9cef58bbf4
Dexter <karl.dexter@intel.com>
2018-10-03 16:22:26 +0100

    updated home.html to render input and associated tests

--- functional_tests.py
+++ functional_tests.py
@@ -12,7 +12,7 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://localhost:8000')
+        self.browser.get('http://127.0.0.1:8000')
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
@@ -21,15 +21,13 @@ class NewVisitorTest(unittest.TestCase):
         self.assertEqual(inputbox.get_attribute('placeholder'),
                          'Enter a to-do item')
 
-        inputbox.send_keys('Buy feathers')
-        inputbox.send_keys('Keys.ENTER')
+        inputbox.send_keys("Buy peacock feathers")
+        inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy feathers' for row in rows),
-            "New item didnt appear in table")
+        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
 
         self.fail('Finish the test!')
 
