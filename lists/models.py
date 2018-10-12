443c20189608723640751f46a3cc082b72ec411a
Karl Dexter <karl.dexter@intel.com>
2018-10-12 16:43:11 +0100

    cant remember, something about new lists

--- lists/models.py
+++ lists/models.py
@@ -3,3 +3,6 @@ from django.db import models
 # Create your models here.
 class Item(models.Model):
     text = models.TextField(default='')
+    item = models.ForeignKey(List, default=None)
+
+class List(models.Model):
--- lists/tests.py
+++ lists/tests.py
@@ -1,22 +1,26 @@
-from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
-
-from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your tests here.
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 
     def test_saving_an_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -24,7 +28,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
 
 class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
